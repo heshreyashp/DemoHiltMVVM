0163932d05ae546d689167dabf2bcb0770cf339e
Shreyash <shreyash.p@hubengage.com>
2023-09-23 21:22:40 +0530

    added recycler view

---
+++ app/src/main/java/java/com/cheezycode/daggermvvm/CustomAdapter.kt
@@ -0,0 +1,42 @@
+package java.com.cheezycode.daggermvvm
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.cheezycode.daggermvvm.R
+import com.cheezycode.daggermvvm.models.Product
+
+class CustomAdapter(private val mList: ArrayList<String>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    // create new views
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // inflates the card_view_design view
+        // that is used to hold list item
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    // binds the list items to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val ItemsViewModel = mList[position]
+
+
+        // sets the text to the textview from our itemHolder class
+        holder.textView.text = ItemsViewModel.toString()
+
+    }
+
+    // return the number of the items in the list
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    // Holds the views for adding it to image and text
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val textView: TextView = itemView.findViewById(R.id.itemName)
+    }
+}
--- app/src/main/java/java/com/cheezycode/daggermvvm/MainActivity.kt
+++ app/src/main/java/java/com/cheezycode/daggermvvm/MainActivity.kt
@@ -2,19 +2,21 @@ package com.cheezycode.daggermvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.cheezycode.daggermvvm.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.com.cheezycode.daggermvvm.CustomAdapter
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     lateinit var mainViewModel: MainViewModel
 
-    private val products: TextView
-    get() = findViewById(R.id.products)
+    private val recyclerview: RecyclerView
+        get() = findViewById(R.id.products)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +24,21 @@ class MainActivity : AppCompatActivity() {
 
         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
+        // this creates a vertical layout Manager
+        recyclerview.layoutManager = LinearLayoutManager(this)
         mainViewModel.productsLiveData.observe(this, Observer {
-           products.text =  it.joinToString { x -> x.title + "\n\n" }
+            //products.text =  it.joinToString { x -> x.title + "\n\n" }
+
+            var strings: ArrayList<String> = ArrayList()
+            for (i in it.indices) {
+                strings.add(it[i].title)
+            }
+
+            val adapter = CustomAdapter(strings)
+            recyclerview.adapter = adapter
         })
+
+
     }
 
 }
